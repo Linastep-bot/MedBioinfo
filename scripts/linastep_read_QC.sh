0a88af7263274246e643b3a4147f245ed2d1a16d
Lina Stepanauskaite <linastep@core-login2.cluster.france-bioinformatique.fr>
2023-05-23 19:43:50 +0200

    seqkit stats, prep for duplicate analysis

--- scripts/linastep_read_QC.sh
+++ scripts/linastep_read_QC.sh
@@ -11,7 +11,13 @@ mkdir ../data/sra_fastq
 module load sra-tools
 #Download the files with accessions
 cat linastep_run_accessions.txt | srun --cpus-per-task=1 --time=01:00:00 xargs fastq-dump --split-3 --gzip --readids --outdir ../data/sra_fastq/ --disable-multithreading
-
+#count how many reads were found per FASTQ file
+find ../data/sra_fastq/ -name "*.fastq.gz" | xargs zgrep -c "^@"
+#compare number of acquired reads with seqkit
+module load seqkit/
+srun --cpus-per-task=4 --time=01:00:00 seqkit stats -j 4 ../data/sra_fastq/*.fastq.gz > linastep_seqkit_stats.txt
+sqlite3 -batch ../../../pascal/central_database/sample_collab.db "SELECT * FROM sample_annot LEFT JOIN sample2bioinformatician ON sample_annot.patient_code=sample2bioinformatician.patient_code WHERE username = 'linastep';" #after manual comparison number of reads correspond; the reads are trimmed as normally Illumina reads are 50-300 bp length and the reads that we have are 35-151 bp length. 
+#check if FASTQ files have been de-replicated. 
 
 echo "Finished"
 date
